a9affd183d61251453571d0b6a3f6d88d2659325
Kumari <preeti.ac.kumari@accenture.com>
2021-06-01 17:25:41 -0400

    TwitterQA external task worker

---
+++ rejection-external/src/main/java/com/camunda/rejectionexternal/Application.java
@@ -0,0 +1,39 @@
+package com.camunda.rejectionexternal;
+
+import org.camunda.bpm.client.ExternalTaskClient;
+import org.camunda.bpm.client.topic.TopicSubscriptionBuilder;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public  class Application {
+    public static void main(String[] args){
+
+        //add or call respective layers to get the task done
+
+        // bootstrap the client
+        ExternalTaskClient client = ExternalTaskClient.create()
+                .baseUrl("http://localhost:8080/engine-rest")
+                .asyncResponseTimeout(20000)
+                .lockDuration(10000)
+                .maxTasks(1)
+                .build();
+
+        // subscribe to the topic
+        TopicSubscriptionBuilder subscriptionBuilder = client
+                .subscribe("notification");
+
+
+        // handle job
+        subscriptionBuilder.handler((externalTask, externalTaskService) -> {
+            String content = externalTask.getVariable("content");
+            System.out.println("Sorry, your tweet has been rejected: " + content);
+            Map<String, Object> variables = new HashMap<String, Object>();
+            variables.put("notficationTimestamp", new Date());
+            externalTaskService.complete(externalTask, variables);
+        });
+
+        subscriptionBuilder.open();
+    }
+}
